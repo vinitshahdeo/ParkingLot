5937a01d7b874b798900a76ab493cb7d53997176
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-17 21:42:38 +0530

    implemented the core functionality of ParkingLot class

--- src/modules/parkingLot.js
+++ src/modules/parkingLot.js
@@ -2,41 +2,123 @@
  * @description a base class for Parking lot
  */
 class ParkingLot {
-	constructor () {
 
-    }
-    
-	createParkingLot () {
-        
+    // will initialize it later, first writing the business logic
+    constructor () {
+        this.totalParkings = 0;
     }
 
-    parkCar () {
-    	
+    createParkingLot (input) {
+        var totalParkings = parseInt(input.split(" ")[1]);
+        return totalParkings;
     }
 
-    leaveCar () {
-    	
+    parkCar (totalParkings, parkingArr, len, input) {
+        if(totalParkings > 0){
+            if(this.findNearestAvailableSlot(parkingArr) == true){
+                    for(var i=0;i<len;i++){
+                        if(parkingArr[i][i] == null){
+                            var inp = input.split(" ")[1] + ":" + input.split(" ")[2];
+                        parkingArr[i][i] = inp;
+                        i = i + 1;
+                        return i;
+                        }
+                    }
+                }else{
+                    return null;
+                }
+            }else{
+                return null;
+            }
     }
 
-    getParkingStatus () {
-    	
+    leaveCar (totalParkings, parkingArr, input) {
+        if(totalParkings > 0) {
+            var index = input.split(" ")[1] - 1;
+            if (index > -1 && index <= parkingArr.length) {
+                parkingArr[index][index] = null;
+                index = index + 1;
+                return index;
+            }
+        } 
+        else {
+            return null;
+        }
     }
 
-    getCarsWithSameColor () {
-    	
+    getParkingStatus (totalParkings, parkingArr){
+        var arr = new Array();
+        if(totalParkings > 0){
+            arr.push("Slot No. Registration No. Color ");
+            for(var i=0; i<parkingArr.length;i++){
+                if(parkingArr[i][i] != null){
+                    var e = i + 1;
+                    arr.push(e + ".  " + parkingArr[i][i].split(":")[0] + "  " + parkingArr[i][i].split(":")[1]);
+                }
+            }
+            return arr;
+        }
+        else {
+            return [];
+        }
     }
 
-    getSlotsWithSameColorCar () {
-    	
+    getCarsWithSameColor (totalParkings, parkingArr, input){
+        if(totalParkings > 0){
+            var displayArr = new Array();
+            for(var i=0; i< parkingArr.length; i++){
+                if(parkingArr[i][i] && parkingArr[i][i].split(":")[1] == input.split(" ")[1]){
+                    displayArr.push(parkingArr[i][i].split(":")[0]);
+                }
+            }
+            return displayArr.join();
+        }
+        else {
+            return null;
+        }
     }
 
-    getCarNumberBySlot () {
-		
+    getSlotsWithSameColorCar( totalParkings, parkingArr, input) {
+        if(totalParkings > 0){
+            var displayArr = new Array();
+            for(var i=0; i< parkingArr.length; i++){
+                if(parkingArr[i][i] && parkingArr[i][i].split(":")[1] == input.split(" ")[1]){
+                    displayArr.push(i+1);
+                }
+            }
+            return displayArr.join();
+        }
+        else {
+            return null;
+        }
     }
 
-	findNearestAvailableSlot () {
-		
-	}
+    getCarNumberBySlot (totalParkings, parkingArr, input){
+        if(totalParkings > 0){
+            var ele;
+            for(var i=0; i< parkingArr.length; i++){
+                if(parkingArr[i][i] && parkingArr[i][i].split(":")[0] == input.split(" ")[1]){
+                    ele = i + 1;
+                }else{
+                    ele = "Not found";
+                }
+            }
+            return ele;
+        }
+        else {
+            return null;
+        }
+    }
+
+    findNearestAvailableSlot(parkingArr){
+        var ele = false;
+        for(var i=0; i<parkingArr.length; i++){
+            if(parkingArr[i][i] == null){
+                ele = true;
+            }
+        }
+        return ele;
+    }
 }
 
 module.exports = ParkingLot;

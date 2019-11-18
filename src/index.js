6062501014ba884b0aabc52f2b2007314cd6beb6
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-18 21:22:47 +0530

    added business logic for parking lot problem - implemented multiple functions, create/park/leave/find etc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,116 @@
-// driver file
+require('events').EventEmitter.defaultMaxListeners = 0
+
+var http = require('http');
+var fs = require('fs');
+var elements = process.argv;
+var rl = require("readline");
+
+var Parking = require('./modules/parkingLot.js');
+
+var parkingLot = new Parking();
+
+
+
+var totalParkings = 0;
+var parkingArr = new Array();
+
+
+// to avoid memory leaks
+process.setMaxListeners(0);
+
+
+// TODO: What if parking lot is not created
+// TODO: Car with same numbers
+
+if (elements[elements.length - 1] == 'true'){
+	interact();
+} else{
+	fs.readFile(elements[2], 'utf-8', function(err, data) {
+	    var arr = data.split("\n");
+	   	for(var i=0; i < arr.length; i++){
+			commands(arr[i]);
+	   	}
+	});
+}
+
+function interact(){
+	if(elements[elements.length - 1] == 'true'){
+		var prompts = rl.createInterface({ input: process.stdin, output: process.stdout, terminal: false });
+		
+		prompts.question("Input: ", function (data) {
+		    commands(data);
+		});
+	}
+}
+
+function commands(input){
+	var n = input.split(" ")[0];
+	switch (n) {
+	    case "create_parking_lot":
+	        totalParkings = parkingLot.createParkingLot(input);
+	        for(var i=0; i < totalParkings; i++){
+	        	var obj = new Object();
+	        	//obj[parseInt(i)] = null;
+	        	parkingArr.push(null);
+	        }
+	        console.log("Created a parking lot with " + totalParkings  + " slots.");
+	        break;
+	    case "park":
+        	var len = parkingArr.length;
+        	var slotNumber = parkingLot.parkCar(totalParkings, parkingArr, len, input);
+        	if(slotNumber){
+        		console.log("Allocated slot number: " + slotNumber);
+        	}else{
+        		console.log("Sorry, parking lot is full");
+        	}
+	        break;
+	    case "leave":
+	    	var slotNumber = parkingLot.leaveCar(totalParkings, parkingArr, input);
+        	if(slotNumber){
+        		console.log("Slot number " + slotNumber + " is free.");
+        	}else{
+        		console.log("Sorry, parking lot is full");
+        	}
+			break;
+	    case "status":
+	    	var values = parkingLot.getParkingStatus(totalParkings, parkingArr);
+        	if(values.length > 1){
+        		console.log(values.join("\n"));
+			}
+			else{
+        		console.log("Sorry, parking lot is empty"); // what if it's empty
+        	}
+	        break;
+	    case "registration_numbers_for_cars_with_colour":
+	    	var regNum = parkingLot.getCarsWithSameColor(totalParkings, parkingArr, input);
+        	if(regNum){
+        		console.log(regNum);
+        	}else{
+        		console.log("Sorry, Car with given color is not found");
+        	}
+	        break;
+	    case "slot_numbers_for_cars_with_colour":
+	    	var slotNumber = parkingLot.getSlotsWithSameColorCar(totalParkings, parkingArr, input);
+        	if(slotNumber){
+        		console.log(slotNumber);
+			}
+			else {
+        		console.log("Sorry, Car with given color is not found");
+        	}
+	        break;
+	    case "slot_number_for_registration_number":
+	    	var slotNumber = parkingLot.getSlotByCarNumber(totalParkings, parkingArr, input);
+        	if(slotNumber){
+        		console.log(slotNumber);
+        	} else {
+        		console.log("Sorry, Car with given registration number is not found");
+        	}
+			break;
+		case 'exit': 
+			process.exit(0);
+		default: 
+			console.log(n, 'is not a recognized command');
+			break;
+	}
+	interact();
+}
--- src/modules/parkingLot.js
+++ src/modules/parkingLot.js
@@ -1,124 +1,118 @@
+var Car = require('./car.js');
+
 /**
  * @description a base class for Parking lot
  */
 class ParkingLot {
-
-    // will initialize it later, first writing the business logic
-    constructor () {
-        this.totalParkings = 0;
-    }
-
-    createParkingLot (input) {
+	// will initialize it later, first writing the business logic
+	constructor () {
+		this.totalParkings = 0;
+	}
+	createParkingLot (input) {
         var totalParkings = parseInt(input.split(" ")[1]);
         return totalParkings;
     }
-
     parkCar (totalParkings, parkingArr, len, input) {
-        if(totalParkings > 0){
-            if(this.findNearestAvailableSlot(parkingArr) == true){
-                    for(var i=0;i<len;i++){
-                        if(parkingArr[i][i] == null){
-                            var inp = input.split(" ")[1] + ":" + input.split(" ")[2];
-                        parkingArr[i][i] = inp;
-                        i = i + 1;
-                        return i;
-                        }
-                    }
-                }else{
-                    return null;
-                }
-            }else{
-                return null;
-            }
+    	if(totalParkings > 0){
+			var car, carNumber, carColor;
+	    	if(this.findNearestAvailableSlot(parkingArr) == true){
+		  		for(var i=0;i<len;i++){
+		  			if(parkingArr[i] == null){
+						  // var inp = input.split(" ")[1] + ":" + input.split(" ")[2];
+						carNumber = input.split(" ")[1];
+						carColor = input.split(" ")[2]
+						car = new Car(carNumber, carColor);
+						parkingArr[i] = car
+						i = i + 1;
+						return i;
+		  			}
+		  		}
+		  	}else{
+		  		return null;
+		  	}
+	  	}else{
+	  		return null;
+	  	}
     }
-
-    leaveCar (totalParkings, parkingArr, input) {
-        if(totalParkings > 0) {
-            var index = input.split(" ")[1] - 1;
-            if (index > -1 && index <= parkingArr.length) {
-                parkingArr[index][index] = null;
-                index = index + 1;
-                return index;
-            }
-        } 
-        else {
-            return null;
-        }
+    leaveCar (totalParkings, parkingArr, input){
+    	if(totalParkings > 0){
+	    	var index = input.split(" ")[1] - 1;
+		    if (index > -1 && index <= parkingArr.length) {
+			    parkingArr[index] = null;
+			    index = index + 1;
+			    return index;
+			}
+		}
+		else {
+			return null;
+		}
     }
-
     getParkingStatus (totalParkings, parkingArr){
-        var arr = new Array();
-        if(totalParkings > 0){
-            arr.push("Slot No. Registration No. Color ");
-            for(var i=0; i<parkingArr.length;i++){
-                if(parkingArr[i][i] != null){
-                    var e = i + 1;
-                    arr.push(e + ".  " + parkingArr[i][i].split(":")[0] + "  " + parkingArr[i][i].split(":")[1]);
-                }
-            }
-            return arr;
-        }
-        else {
-            return [];
-        }
+    	var arr = new Array();
+    	if(totalParkings > 0) {
+			arr.push("Slot No. Registration No. Color ");
+			// use binary search here
+        	for(var i=0; i<parkingArr.length;i++){
+        		if(parkingArr[i] != null){
+        			var e = i + 1;
+        			arr.push(e + ".  " + parkingArr[i].NUMBER + "  " + parkingArr[i].COLOR);
+        		}
+        	}
+        	return arr;
+		}else{
+			return [];
+		}
     }
-
     getCarsWithSameColor (totalParkings, parkingArr, input){
-        if(totalParkings > 0){
-            var displayArr = new Array();
-            for(var i=0; i< parkingArr.length; i++){
-                if(parkingArr[i][i] && parkingArr[i][i].split(":")[1] == input.split(" ")[1]){
-                    displayArr.push(parkingArr[i][i].split(":")[0]);
-                }
-            }
-            return displayArr.join();
-        }
-        else {
-            return null;
-        }
+		//TODO what if car of particular color is not present
+    	if(totalParkings > 0){
+	        var displayArr = new Array();
+	        for(var i=0; i< parkingArr.length; i++){
+	        	if(parkingArr[i] && parkingArr[i].COLOR.toLowerCase() == input.split(" ")[1].toLowerCase()){
+	        		displayArr.push(parkingArr[i].NUMBER);
+	        	}
+	        }
+    		return displayArr.join(", ");
+		} else{
+			return null;
+		}
     }
-
     getSlotsWithSameColorCar( totalParkings, parkingArr, input) {
-        if(totalParkings > 0){
-            var displayArr = new Array();
-            for(var i=0; i< parkingArr.length; i++){
-                if(parkingArr[i][i] && parkingArr[i][i].split(":")[1] == input.split(" ")[1]){
-                    displayArr.push(i+1);
-                }
-            }
-            return displayArr.join();
-        }
-        else {
-            return null;
-        }
+    	if(totalParkings > 0){
+	    	var displayArr = new Array();
+	        for(var i=0; i< parkingArr.length; i++){
+	        	if(parkingArr[i] && parkingArr[i].COLOR.toLowerCase() == input.split(" ")[1].toLowerCase()){
+	        		displayArr.push(i+1);
+	        	}
+	        }
+        	return displayArr.join(', ');
+        }else{
+			return null;
+		}
     }
-
-    getCarNumberBySlot (totalParkings, parkingArr, input){
-        if(totalParkings > 0){
-            var ele;
-            for(var i=0; i< parkingArr.length; i++){
-                if(parkingArr[i][i] && parkingArr[i][i].split(":")[0] == input.split(" ")[1]){
-                    ele = i + 1;
-                }else{
-                    ele = "Not found";
-                }
-            }
-            return ele;
-        }
-        else {
-            return null;
-        }
-    }
-
-    findNearestAvailableSlot(parkingArr){
-        var ele = false;
-        for(var i=0; i<parkingArr.length; i++){
-            if(parkingArr[i][i] == null){
-                ele = true;
-            }
-        }
-        return ele;
-    }
+    getSlotByCarNumber (totalParkings, parkingArr, input){
+		// TODO:  What parking lot is empty
+		if(totalParkings > 0){
+	    	var ele = "Not found";
+	        for(var i=0; i< parkingArr.length; i++){
+	        	if(parkingArr[i] && parkingArr[i].NUMBER == input.split(" ")[1]){
+	        		ele = i + 1;
+	        	}
+	        }
+        	return ele;
+        } else {
+			return null;
+		}
+	}
+	findNearestAvailableSlot(parkingArr){
+		var ele = false;
+		for(var i=0; i<parkingArr.length; i++){
+			if(parkingArr[i] == null){
+				ele = true;
+			}
+		}
+		return ele;
+	}
 }
 
 module.exports = ParkingLot;

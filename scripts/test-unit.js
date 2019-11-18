85e46a830b7ceb2ac2e725bd920e4765b87f1d35
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-19 03:17:57 +0530

    added npm script for unit-tests

--- scripts/test-unit.js
+++ scripts/test-unit.js
@@ -1,1 +1,9 @@
-// unit tests for different functions
+// Running unit tests for different functions
+const sh = require('shelljs');
+
+sh.exec('mocha tests/specs.js', (err, stdout, stderr) => {
+    if (err) {
+        console.log('Error running unit tests');
+    }
+    console.log('Running unit tests');
+});

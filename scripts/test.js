13509187d4710ba349455b5cb3e01ad619d1b5ab
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-19 03:19:17 +0530

    added script for npm run test

--- scripts/test.js
+++ scripts/test.js
@@ -1,1 +1,44 @@
 // combination of all tests
+const sh = require('shelljs'),
+    chalk = require('chalk'),
+    async = require('async');
+
+async.series([
+    function runSystemTests (next) {
+        console.log('Running system tests');
+        sh.exec('npm run test-system', (err, stdout, stderr) => {
+            if (err) {
+                console.log('Error running system tests');
+                return next(err);
+            }
+            return next(null);
+        });
+    },
+    function runLintTests (next) {
+        console.log('Running lint tests');
+        sh.exec('npm run test-lint', (err, stdout, stderr) => {
+            if (err) {
+                console.log('Error running ESLint tests');
+                return next(err);
+            }
+            return next(null);
+        });
+    },
+    function runUnitTests (next) {
+        console.log('Running unit tests');
+        sh.exec('npm run test-unit', (err, stdout, stderr) => {
+            if (err) {
+                console.log(chalk.red('Error running unit tests'));
+                return next(err);
+            }
+            return next(null);
+        });
+    }
+], (err) => {
+    if (err) {
+        console.log(chalk.red('Tests failed due to error occured '));
+    }
+    else {
+        console.log(chalk.green.bold('All the tests has passed successfully'));
+    }
+});

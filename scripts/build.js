1ff8d596a36f19528e2e7020cb6542ffb8306bef
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-19 03:53:44 +0530

    added scripts for building parking lot executable for the host platform

--- scripts/build.js
+++ scripts/build.js
@@ -1,2 +1,26 @@
-// script to build the executable.
-// executable will be present in bin/ folder
+const sh = require('shelljs'),
+  fs = require('fs'),
+  chalk = require('chalk'),
+  pkg = require('../package.json'),
+  host = require('./utils/findHost')();
+
+console.log(chalk.yellow.bold('Packaging the Parking Lot Console Application for host platform'));
+
+sh.exec('pkg ./src/index.js -t host --out-path bin', (code, stderr, stdout) => {
+    if (code !== 0 && stderr) {
+        console.log(chalk.red.bold('Error occured while packaging Parking Lot'));
+        return 0;
+    }
+    else {
+        fs.rename('./bin/index', './bin/parking_lot', function (err) {
+            if (err) {
+                console.log(chalk.red.bold('Error occured while renaming the executable'));
+            }
+            else {
+                console.log(chalk.green.bold('Parking Lot (version : ' + pkg.version + ') is created successfully for ' + host + ' in "bin/" folder'));
+            }
+        });
+    }
+});
+
+
---
+++ scripts/utils/findHost.js
@@ -0,0 +1,18 @@
+/**
+ * @description used to find the the platform the Parking Lot executable is being built
+ */
+module.exports = function () {
+    var opsys = process.platform;
+  
+    if (opsys === 'darwin') {
+      opsys = 'MacOS';
+    }
+    else if (opsys === 'win32' || opsys === 'win64') {
+      opsys = 'Windows';
+    }
+    else if (opsys === 'linux') {
+      opsys = 'Linux';
+    }
+  
+    return opsys;
+};

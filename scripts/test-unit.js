f993c55a5b334333cdb4a4cdb50028f598d27b48
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-21 22:42:07 +0530

    change in log statements

--- scripts/test-unit.js
+++ scripts/test-unit.js
@@ -5,5 +5,5 @@ sh.exec('mocha tests/specs.js', (err, stdout, stderr) => {
     if (err) {
         console.log('Error running unit tests');
     }
-    console.log('Running unit tests');
+    console.log('Running unit tests...');
 });

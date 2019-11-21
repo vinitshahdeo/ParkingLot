8dbc7b32426d3cc5a6fd5f2c8f4e6e601173e736
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-21 21:24:34 +0530

    added missing comments for readablity

--- src/modules/car.js
+++ src/modules/car.js
@@ -5,7 +5,7 @@
  */
 class Car {
     constructor (NUMBER, COLOR) {
-        this.NUMBER = NUMBER;
+        this.NUMBER = NUMBER; // unique propery of an instance of car class
         this.COLOR = COLOR;
     }
 

0e1b907aed6802cef2b1a2c1bd36b3c09e0f7231
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-21 22:35:04 +0530

    change in console log statements of create-docs script

--- scripts/create-docs.js
+++ scripts/create-docs.js
@@ -1,8 +1,6 @@
 const sh = require('shelljs'),
   chalk = require('chalk');
 
-console.log(chalk.yellow.bold('Creating documentation using JSDoc'));
-
 /**
  * @type {Array}
  * @description The list of files to include into documentation
@@ -14,13 +12,15 @@ var LIST_OF_FILES = [
 
 const command = 'jsdoc ' + LIST_OF_FILES.join(' '); // combining all the files
 
+console.log(chalk.yellow.bold('Creating documentation using JSDoc...\n'));
+
 sh.exec(command, (code, stderr, stdout) => {
     if (code !== 0 && stderr) {
-        console.log(chalk.red.bold('Error occured while packaging Parking Lot'));
+        console.log(chalk.red.bold('Error occured while packaging Parking Lot\n'));
         return 0;
     }
     else {
-        console.log(chalk.green.bold('Documentation is created inside out/ folder'));
-        console.log(chalk.magenta.bold('Go to out/ folder and open global.html'));
+        console.log(chalk.green.bold('Success! Documentation is created inside out/ folder\n'));
+        console.log(chalk.bgMagenta.bold('Go to out/ folder and open global.html\n'));
     }
 });

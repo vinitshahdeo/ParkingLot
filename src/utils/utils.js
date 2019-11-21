56e117af89bb72d64aee4991ec214344c86ca833
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-21 22:43:26 +0530

    added util functions, to be used in future

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,32 @@
-// this will contain all the utility functions
+// This file contains all the pure utility functions
+
+/**
+ * 
+ * @param {Array} arr the array in which the function will search for the element
+ * @param {Number} element element to be searched
+ * @param {Number} start starting index of the array
+ * @param {Number} end last index of the array
+ * @description This is an implementation of Binary search algorithm.
+ * We might use this for searching operation
+ */
+const binarySearch = function (arr, element, start, end) { 
+    if (start > end) {
+        return false; 
+    }
+    
+    var mid = Math.floor((start + end)/2); 
+    
+    if ( arr[mid] === element) {
+        return true; 
+    }
+          
+    if(arr[mid] > element)  {
+        return binarySearch(arr, element, start, mid-1); 
+    }
+        
+    else {
+        return binarySearch(arr, element, mid+1, end); 
+    }       
+}
+
+module.exports = binarySearch;

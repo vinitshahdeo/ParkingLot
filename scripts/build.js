12276e0d796cf55593e6a5a8280ae6e5db6987f7
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2019-11-22 16:24:14 +0530

    fixed typo in console statements

--- scripts/build.js
+++ scripts/build.js
@@ -8,14 +8,14 @@ console.log(chalk.yellow.bold('Packaging the Parking Lot Console Application for
 
 sh.exec('pkg ./src/index.js -t host --out-path bin', (code, stderr, stdout) => {
     if (code !== 0 && stderr) {
-        console.log(chalk.red.bold('Error occured while packaging Parking Lot'));
+        console.log(chalk.red.bold('Error occurred while packaging Parking Lot'));
         return 0;
     }
     else {
         // renaming it as per assignment instructions
         fs.rename('./bin/index', './bin/parking_lot', function (err) {
             if (err) {
-                console.log(chalk.red.bold('Error occured while renaming the executable'));
+                console.log(chalk.red.bold('Error occurred while renaming the executable'));
             }
             else {
                 console.log(chalk.green.bold('Parking Lot (version : ' + pkg.version + ') is created successfully for ' + host + ' in "bin/" folder\n'));
